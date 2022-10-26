2f7dd21af057693584cb7eda84d1f7a97afe6b8c
Alex Hoffman <alxhoff@gmail.com>
2022-10-26 17:07:59 +0200

    coding challenge 2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
 
-int main(int argc, char* argv[])
-{
-     printf("Hello Epsl");
+
+
+#include <stdio.h>
+#include <espl_lib.h>
+int main() {
+int num;
+char chr;
+  
+do {
+   printf("Give me a number\n");
+   scanf("%d",&num);
+  printf ("%s\n" ,num_to_words( num));
+printf("Do you want to exit the programm Y/N \n");
+scanf("%s", &chr);
+}
+ while (chr == 'y' || chr == 'Y');
+
 }
